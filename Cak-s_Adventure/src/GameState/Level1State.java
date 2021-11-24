b2ca9febc1618bf5d683747428815cd43e22c1db
tegar-ganang <tegarsatrio10@gmail.com>
2021-11-24 20:17:07 +0700

    add level 1 state class

--- Cak-s_Adventure/src/GameState/Level1State.java
+++ Cak-s_Adventure/src/GameState/Level1State.java
@@ -2,13 +2,14 @@ package GameState;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-
 import Main.GamePanel;
+import TileMap.Background;
 import TileMap.TileMap;
 
 public class Level1State extends GameState {
 
 	private TileMap tileMap;
+	private Background bg;
 
 	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
@@ -20,15 +21,16 @@ public class Level1State extends GameState {
 		tileMap.loadTiles(""); // input tile picture here
 		tileMap.loadMap(""); // input map here
 		tileMap.setPosition(0, 0);
+		
+		bg = new Background("", 0.1); // input background here
 	}
 
 	public void update() {
 	}
 
 	public void draw(Graphics2D g) {
-		// Clear screen
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		// Draw bg
+		bg.draw(g);
 		
 		// Draw tile map
 		tileMap.draw(g);
