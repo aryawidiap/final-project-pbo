c216b6fb74412751e845cabbd1f381694d8239d6
aryawidiap <aryawidiaputra2002@gmail.com>
2021-11-26 00:16:25 +0800

    feat add Player object feat update method feat implement key event

--- Cak-s_Adventure/src/GameState/Level1State.java
+++ Cak-s_Adventure/src/GameState/Level1State.java
@@ -2,6 +2,9 @@ package GameState;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+import Entity.Player;
 import Main.GamePanel;
 import TileMap.Background;
 import TileMap.TileMap;
@@ -10,6 +13,7 @@ public class Level1State extends GameState {
 
 	private TileMap tileMap;
 	private Background bg;
+	private Player player;
 
 	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
@@ -21,31 +25,84 @@ public class Level1State extends GameState {
 		tileMap.loadTiles(""); // input tile picture here
 		tileMap.loadMap(""); // input map here
 		tileMap.setPosition(0, 0);
-		
+
 		bg = new Background("", 0.1); // input background here
+		player = new Player(tileMap);
+		player.setPosition(100, 100); // set player position to desired starting point
+
 	}
 
 	public void update() {
+		// Update player
+		player.update();
+		tileMap.setPosition(GamePanel.WIDTH / 2 - player.getx(), GamePanel.HEIGHT / 2 - player.gety());
 	}
 
 	public void draw(Graphics2D g) {
 		// Draw bg
 		bg.draw(g);
-		
+
 		// Draw tile map
 		tileMap.draw(g);
+
+		// Draw player
+		player.draw(g);
 	}
 
 	@Override
 	public void keyPressed(int k) {
-		// TODO Auto-generated method stub
-
+		if (k == KeyEvent.VK_LEFT) {
+			player.setLeft(true);
+		}
+		if (k == KeyEvent.VK_RIGHT) {
+			player.setRight(true);
+		}
+		if (k == KeyEvent.VK_UP) {
+			player.setUp(true);
+		}
+		if (k == KeyEvent.VK_DOWN) {
+			player.setDown(true);
+		}
+		if (k == KeyEvent.VK_W) {
+			player.setJumping(true);
+		}
+		if (k == KeyEvent.VK_E) {
+			player.setGliding(true);
+		}
+		if (k == KeyEvent.VK_R) {
+			player.setScratching();
+		}
+		if (k == KeyEvent.VK_F) {
+			player.setFiring();
+		}
 	}
 
 	@Override
 	public void keyReleased(int k) {
-		// TODO Auto-generated method stub
-
+		if (k == KeyEvent.VK_LEFT) {
+			player.setLeft(false);
+		}
+		if (k == KeyEvent.VK_RIGHT) {
+			player.setRight(false);
+		}
+		if (k == KeyEvent.VK_UP) {
+			player.setUp(false);
+		}
+		if (k == KeyEvent.VK_DOWN) {
+			player.setDown(false);
+		}
+		if (k == KeyEvent.VK_W) {
+			player.setJumping(false);
+		}
+		if (k == KeyEvent.VK_E) {
+			player.setGliding(false);
+		}
+		if (k == KeyEvent.VK_R) {
+			player.setScratching();
+		}
+		if (k == KeyEvent.VK_F) {
+			player.setFiring();
+		}
 	}
 
 }
