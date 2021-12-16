984840afcbc40a45ad0d1de00f4127f03f9c0e6b
aryawidiap <aryawidiaputra2002@gmail.com>
2021-12-16 20:13:00 +0800

    fix add hierarchy and debug movement

--- Cak-s_Adventure/src/Entity/Enemy.java
+++ Cak-s_Adventure/src/Entity/Enemy.java
@@ -2,37 +2,20 @@ package Entity;
 
 import TileMap.TileMap;
 
-public class Enemy extends MapObject {
+public abstract class Enemy extends Actor {
 	
-	protected int health;
-	protected int maxHealth;
-	protected boolean dead;
 	protected int damage;
 	
-	protected boolean flinching;
-	protected long flinchTimer;
 	
-	
-	public Enemy(TileMap tm) {
-		super(tm);
-	}
-	
-	public boolean isDead() {
-		return dead;
+	public Enemy(TileMap tm, int maxHealth, int width, int height, int cwidth, int cheight, double moveSpeed, double maxSpeed, double fallSpeed, double maxFallSpeed, int damage) {
+		super(tm, maxHealth, width, height, cwidth, cheight, moveSpeed, maxSpeed, fallSpeed, maxFallSpeed);
+		this.damage = damage;
 	}
 	
 	public int getDamage() {
 		return damage;
 	}
 	
-	public void hit(int damage) {
-		if(dead || flinching) return;
-		health -= damage;
-		if(health < 0) health=0;
-		if(health == 0) dead = true;
-		flinching = true;
-		flinchTimer = System.nanoTime();
-	}
 	
 	public void update() {
 		
